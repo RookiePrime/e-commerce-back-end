45c9bc9207ada66b0a1da28be5d1fd08140ac6c0
Kiefer Levine <kiefer_8@hotmail.com>
2021-07-18 13:12:20 -0400

    Added basic CRUD routes

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
+const seedCategories = require('./seeds/category-seeds');
+const seedTags = require('./seeds/tag-seeds');
+const seedProducts = require('./seeds/product-seeds');
+const seedProductTags = require('./seeds/product-tag-seeds');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,3 +19,8 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
+
+// seedCategories();
+// seedTags();
+// seedProducts();
+// seedProductTags();
